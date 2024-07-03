d1db8d68312be7f79c60c2c7fddb1fb156a10465
Infrector <infrector@gmail.com>
2024-07-03 22:58:29 +0200

    Updated index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ navLinks.forEach(link => {
         navbar.classList.remove('active');
     }
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby98jivGCIaPLxvJjBNJ_J0YW0KQN-SOQ7mtiHXOfAr1qr50DmACZIuwCiuSHmjvOXb-g/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
